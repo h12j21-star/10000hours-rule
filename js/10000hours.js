fd49a564d7d3ae70e83227792f4adb4d9e8ba97b
hyoenji.choi <jh04022@naver.com>
2022-10-28 02:44:08 +0900

    js추가 & 반응형

--- js/10000hours.js
+++ js/10000hours.js
@@ -1,6 +1,24 @@
 // js file
 
 //input으로 받은 text화면에 띄우기
+//let sumit_form = document.querySelector('#res-1hours');
+//let form = document.querySelector('#1hours-form')
+let wanabe = document.querySelector('.re-wanabe');
+let time =  document.querySelector('.re-time');
+let inp_want =  document.querySelector('.inp-want');
+let inp_time = document.querySelector('.inp-time');
+let btn_res = document.querySelector('.btn-res');
+
+function print_TimeWant (){
+  let val_want = inp_want.value;
+  let val_time = inp_time.value;
+  wanabe.textContent=val_want;
+  time.textContent=val_time;
+}
+btn_res.addEventListener('click',(event)=>{
+  event.preventDefault();
+  print_TimeWant ()
+})
 
 
 //버튼 누르면 모달창 나타나고 없어지기
